12202af1e3cee3184f97983990c204953d737599
Iltsukka <113025832+Iltsukka@users.noreply.github.com>
2023-07-14 18:13:57 +0300

    4.12 works

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const config = require('./utils/config')
 const logger = require('./utils/logger')
-
 const express = require('express')
+require('express-async-errors')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
+const errorHandler = require('./utils/errorHandler')
 
 
 
@@ -19,11 +20,6 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
-
-
-
-
-
-
+app.use(errorHandler)
 
 module.exports = app
--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,6 +1,5 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
-const { error } = require('../utils/logger')
 
 blogsRouter.get('/', async (request, response) => {
 
@@ -9,13 +8,10 @@ blogsRouter.get('/', async (request, response) => {
   })
 
 blogsRouter.post('/',  async (request, response) => {
-  
   const blog = new Blog(request.body)
 
   const savedBlog = await blog.save()
   response.status(201).json(savedBlog)
- 
-  
 })
 
 module.exports = blogsRouter
