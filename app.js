fedb13ae423e63d38d29e74f34764a5181f5a43a
Iltsukka <113025832+Iltsukka@users.noreply.github.com>
2023-07-15 18:53:51 +0300

    exercise 4.15

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const errorHandler = require('./utils/errorHandler')
 
 
@@ -20,6 +21,7 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 app.use(errorHandler)
 
 module.exports = app
