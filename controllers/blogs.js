9c83f538bc5c400491176dfc8d294df219cba377
Iltsukka <113025832+Iltsukka@users.noreply.github.com>
2023-08-22 13:45:59 +0300

    exercise 4.22

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,6 +2,7 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
 const jwt = require('jsonwebtoken')
+const userExtractor = require('../utils/middleware').userExtractor
 
 
 
@@ -11,40 +12,38 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs)
   })
 
-blogsRouter.post('/',  async (request, response) => {
-  console.log(request.token)
+blogsRouter.post('/', userExtractor, async (request, response) => {
   const body = request.body
-  const decodedToken = jwt.verify(request.token, process.env.SECRET)
-  if (!decodedToken.id) {
-    return response.status(401).json({error: 'invalid token'})
-  }
+  // const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  // if (!decodedToken.id) {
+  //   return response.status(401).json({error: 'invalid token'})
+  // }
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
     likes: body.likes,
-    user: decodedToken.id
+    user: request.user
 
   })
 
   const savedBlog = await blog.save()
 
-  const person = await User.findById(decodedToken.id)
+  const person = await User.findById(request.user)
   person.blogs = person.blogs.concat(savedBlog._id)
   await person.save()
   response.status(201).json(savedBlog)
 })
 
-blogsRouter.delete('/:id', async (request, response) => {
+blogsRouter.delete('/:id', userExtractor, async (request, response) => {
 
-  const decodedToken = jwt.verify(request.token, process.env.SECRET)
-  if (!decodedToken.id) {
-    return response.status(401).json({error: 'invalid token'})
-  }
-
+  // const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  // if (!decodedToken.id) {
+  //   return response.status(401).json({error: 'invalid token'})
+  // }
   const blog = await Blog.findById(request.params.id)
-  console.log(blog.user.toString(), decodedToken.id.toString())
-  if (blog.user.toString() === decodedToken.id.toString()) {
+  
+  if (blog.user.toString() === request.user.toString()) {
     await Blog.findByIdAndRemove(request.params.id)
     return response.status(204).end()
 
--- utils/middleware.js
+++ utils/middleware.js
@@ -1,3 +1,5 @@
+const jwt = require("jsonwebtoken")
+
 const tokenExtractor = (request, response, next) => {
     let authorization = request.get('authorization')
     if (authorization && authorization.startsWith('Bearer ')) {
@@ -19,4 +21,14 @@ const tokenExtractor = (request, response, next) => {
 //     return null
 //   }
 
-module.exports = {tokenExtractor}
+const userExtractor = (request, response, next) => {
+    console.log(`request.token of userExtraction is ${request.token}`)
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    if (!decodedToken.id) {
+        return response.status(400).json({error: 'invalid token'})
+    }
+    request.user = decodedToken.id
+    next()
+}
+
+module.exports = {tokenExtractor, userExtractor}
